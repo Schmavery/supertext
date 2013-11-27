fbe4c9a8f8ccd07e633bdd71cfccc51d0457e4d3
Schmavery <elflord@live.ca>
2013-11-26 21:08:36 -0500

    Worked on dictView

--- SuperText/dictView.js
+++ SuperText/dictView.js
@@ -2,13 +2,33 @@ $(function() {
   // You know have access to dictController.saveDictionary(dict, callback) and
   // dictController.loadDictionary(callback), for this last one, the callback
   // will receive as argument the dictionary
-  var dictController = require("./dictionaryController.js");
+  loadDict(["cat", "mouse", "dog", "rat", "giraffe", "elephant", "pikachu", "cat", "mouse", "dog", "rat", "giraffe", "elephant", "pikachu"]);
+ // dictController.loadDictionary(loadDict);
+  function loadDict(dict){
+    var wrapper = document.createElement("div");
+    var buttonBar = document.createElement("div");
+    $(wrapper).addClass("wrapper");
+    $(buttonBar).addClass("buttonbar");
+    
+    $(buttonBar).append("HEEEELLLOO OUTT THEEERREEEE....");
+    wrapper.appendChild(buttonBar);
 
+    
 
-  var dict;
-  chrome.storage.sync.get(request.data, function(data) {
-    sendResponse({data: data});
-  });
-
+    for (var i = 0; i < dict.length; i++){
+      var item = document.createElement("div");
+      var chk = $("<input>", {
+        type:"checkbox"
+      }).addClass("chk").appendTo(item);
+//      $(chk).addClass("chk");
+      $(item).addClass("dictItem");
+      $(item).append(dict[i]);
+//      item.appendChild(chk);
+//      console.log(item);
+//      console.log(document.body);
+      wrapper.appendChild(item);
+    }
+    document.body.appendChild(wrapper);
+  }
 
 });
