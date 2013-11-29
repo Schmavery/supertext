bcc723c4dfc8fdeba66a7a93e52d68f9f511ae05
Schmavery <elflord@live.ca>
2013-11-29 01:03:37 -0500

    Added deleting to dictView.  Doesn't sync with dictionary.

--- SuperText/dictView.js
+++ SuperText/dictView.js
@@ -3,36 +3,45 @@ $(function() {
   // dictController.loadDictionary(callback), for this last one, the callback
   // will receive as argument the dictionary
 
+  // This is a local function, no need for require because we're on the extension
+  // side which can have access to the chrome storage area
+  function loadDictionary(sendResponse) {
+    chrome.storage.sync.get(request.data, function(data) {
+      sendResponse({data: data});
+    });
+  }
+
   loadDict(["cat", "mouse", "dog", "rat", "giraffe", "elephant", "pikachu", "cat", "mouse", "dog", "rat", "giraffe", "elephant", "pikachu"]);
- // loadDictionary(loadDict);
+  // loadDictionary(loadDict);
   function loadDict(dict){
     var wrapper = document.createElement("div");
-    var buttonBar = document.createElement("div");
     $(wrapper).addClass("wrapper");
-    $(buttonBar).addClass("buttonbar");
-
-    $(buttonBar).append("HEEEELLLOO OUTT THEEERREEEE....");
-    wrapper.appendChild(buttonBar);
-
-
 
     for (var i = 0; i < dict.length; i++){
       var item = document.createElement("div");
-      var chk = $("<input>", {
-        type:"checkbox"
-      }).addClass("chk").appendTo(item);
+
+
       $(item).addClass("dictItem").append(dict[i]);
+      
+      var btn = $("<input>", {
+        type:"button",
+        value:"DELETE"
+      }).addClass("deletebtn").appendTo(item);
+      
+      btn.on("click", null, btn, editDict);
+
       wrapper.appendChild(item);
     }
     document.body.appendChild(wrapper);
   }
 
+  function editDict(btn){
+    console.log("Editing dict");
+//    var dictItems = document.getElementsByClassName("dictItem");
+    console.log(btn);
+    console.log($(btn.target).parent());
+    $(btn.target).parent().remove();
+  }
+
 });
 
-// This is a local function, no need for require because we're on the extension
-// side which can have access to the chrome storage area
-function loadDictionary(sendResponse) {
-  chrome.storage.sync.get(request.data, function(data) {
-    sendResponse({data: data});
-  });
-}
